660822ab6ed5930bf9339b16dd85d814857fc242
Tnoob-dev <93684341+Tnoob-dev@users.noreply.github.com>
2022-01-15 17:33:06 -0500

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,6 +1,6 @@
 import os
 
 class Creds:
-    api_hash = "578d2817642f3aa3a283efaf49f4ef4e"
-    api_id = int(12845924)
-    bot_token = "2026467093:AAGCKQBm-6MqqyXokaQh-qsY-ZHku2KbrHA"
+    api_hash = None
+    api_id = None
+    bot_token = None
